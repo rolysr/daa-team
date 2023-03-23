a0c8fa14c66ea12f503cf896928d6486c74457f0
rolysr <rolsanchez@yandex.com>
2023-03-23 05:37:22 -0400

    Fix ternary search solution

--- Problema1/Soluciones/greedy_bs_ts.py
+++ Problema1/Soluciones/greedy_bs_ts.py
@@ -16,15 +16,19 @@ def greedy_bs_ts(n: int, hi: list[int], c: int, e, m) -> int:
     hi.sort() # sort the heights
     hi_sum = get_sum_array(hi)
 
-    for height in range(min_h, max_h + 1):
-        result = solve(0, n-1, n, hi_sum, c, e, m, height)
-        answer = min(result, answer)
+    answer = ts_solve(min_h, max_h, n, hi_sum, c, e, m)
 
     return answer
 
 def ts_solve(h1, h2, n, hi_sum, c, e, m):
     if h1 == h2:
         return solve(0, n-1, n, hi_sum, c, e, m, h1)
+    
+    elif h2 - h1 == 1:
+        result_h1 = solve(0, n-1, n, hi_sum, c, e, m, h1)
+        result_h2 = solve(0, n-1, n, hi_sum, c, e, m, h2)
+        answer = min(result_h1, result_h2)
+        return answer
     
     mid1 = h1 + (h2 - h1) // 3
     mid2 = h2 - (h2 - h1) // 3
@@ -33,10 +37,10 @@ def ts_solve(h1, h2, n, hi_sum, c, e, m):
     result_mid2 = solve(0, n-1, n, hi_sum, c, e, m, mid2)
 
     if result_mid1 == result_mid2:
-        return ts_solve(mid1, mid2, n, hi_sum, c, e, m)
+        return ts_solve(mid1, mid2-1, n, hi_sum, c, e, m)
 
     elif result_mid1 < result_mid2:
-        return ts_solve(h1, mid2, n, hi_sum, c, e, m)
+        return ts_solve(h1, mid2-1, n, hi_sum, c, e, m)
     
     else:
-        return ts_solve(mid1, h2, n, hi_sum, c, e, m)
+        return ts_solve(mid1+1, h2, n, hi_sum, c, e, m)
