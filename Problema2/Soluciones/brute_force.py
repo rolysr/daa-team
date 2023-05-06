664335259ebc1b3b4a0c56d7812d8728edbc70c4
Andry Rosquet Rodriguez <andryrosquet@gmail.com>
2023-05-05 21:31:44 -0400

    Fixed_ultimate_hope_so
    I really hope so

--- Problema2/Soluciones/brute_force.py
+++ Problema2/Soluciones/brute_force.py
@@ -16,12 +16,7 @@ def brute_force(n, m, edges, useful_paths_tuples):
 def get_useful_edges(u, v, l, g, edges):
     useful_edges = set()
     paths = []
-    visited = {}
-    for edge in edges:
-        visited[edge] = False
-        u, v, weight = edge
-        visited[(v, u, weight)] = False
-    get_paths(u, v, l, g, visited, 0, [], paths)
+    get_paths(u, v, l, g, 0, [], paths)
     
     for path in paths:
         for edge in path:
@@ -29,24 +24,19 @@ def get_useful_edges(u, v, l, g, edges):
 
     return useful_edges
 
-def get_paths(u, v, l, g, visited, current_cost, current_path, total_paths):
+def get_paths(u, v, l, g, current_cost, current_path, total_paths):
     if current_cost > l:
         return
 
     if u == v and current_cost <= l:
         current_path_cpy = copy.deepcopy(current_path)
         total_paths.append(current_path_cpy)
-        return
     
     for ady in g.adyacents[u]:
         node, weight = ady
-        if not visited[(u, node, weight)] and not visited[(node, u, weight)] and current_cost + weight <= l:
-            visited[(u, node, weight)] = True
-            visited[(node, u, weight)] = True
+        if current_cost + weight <= l:
             current_cost += weight
             current_path.append((u, node))
-            get_paths(node, v, l, g, visited, current_cost, current_path, total_paths)
-            visited[(u, node, weight)] = False
-            visited[(node, u, weight)] = False
+            get_paths(node, v, l, g, current_cost, current_path, total_paths)
             current_cost -= weight
             current_path.pop()
