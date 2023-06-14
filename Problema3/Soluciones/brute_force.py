f2031ae4a807c57fdc486a30354fd3ca666f4d5f
Andry Rosquet Rodriguez <andryrosquet@gmail.com>
2023-06-14 12:33:34 -0400

    Solution_was_fixed

--- Problema3/Soluciones/brute_force.py
+++ Problema3/Soluciones/brute_force.py
@@ -1,19 +1,41 @@
 import copy
-from Soluciones.graph import Graph
 
-def brute_force(n, m, edges):
+
+def brute_force1(n, m, edges):
     bitmask = [True for i in range(m)]
-
+    
     if m == 0 or is_valid_graph(n, m, edges, bitmask):
         return True
-
+    
     for i in range(1, 2**m):
         for j in range(m-1):
-            print(len(bitmask), j)
+            #print(len(bitmask), j)
             bitmask[j] = i & j
         if is_valid_graph(n, m, edges, bitmask):
             return True
 
+    return False
+
+def brute_force(n,m,edges):
+    bitmask = [True for i in range(a[0][1])]
+    if m== 0 or is_valid_graph(n,m,edges,bitmask):
+        print(True)
+    else:
+        print(brute_force_recursive(n,m,edges,bitmask,0))
+
+def brute_force_recursive(n, m, edges,bitmask,arist):
+    if arist== m:
+        print(bitmask)
+        return is_valid_graph(n,m,edges,bitmask)
+    
+    arist+=1
+    if brute_force(n,m,edges,bitmask,arist):
+        return True
+    bitmask[arist-1]=False
+    if brute_force(n,m,edges,bitmask,arist):
+        return True
+    bitmask[arist-1]=True
+    
     return False
 
 def is_valid_graph(n, m, edges, bitmask):
@@ -26,8 +48,13 @@ def is_valid_graph(n, m, edges, bitmask):
             degree_vertex[b] += 1
             if degree_vertex[a] > 3 or degree_vertex[b] > 3:
                 return False
-        
+    count=0   
     for degree in degree_vertex:
         if degree != 3 and degree != 0:
             return False
-    return True
+    for bit in bitmask:
+        if not bit:
+            count+=1
+    if count == m:
+        return False
+    return True      
